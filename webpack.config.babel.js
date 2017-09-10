fe01fe0020949c30dfdbdc93e0193e24b6bc6569
Miguel Palau <mpalauzarza@gmail.com>
2017-09-10 18:32:30 -0500

    try 1

---
+++ webpack.config.babel.js
@@ -0,0 +1,57 @@
+import { join, resolve } from 'path'
+import webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+const devServer = {
+  hot: true,
+  inline: true
+}
+
+const stats = {
+  assets: false,
+  colors: true,
+  version: false,
+  hash: true,
+  timings: true,
+  chunks: false,
+  chunkModules: false
+}
+
+const entry = {
+  index: join(__dirname, 'src', 'index.pug')
+}
+
+const output = {
+  path: resolve(__dirname, 'build'),
+  filename: '[name].js'
+}
+
+const module = {
+  rules: [
+    {
+      test: /\.pug$/,
+      use: ['html-loader', 'pug-html-loader?pretty&exports=false']
+    }
+  ]
+}
+
+const plugins = [
+  new webpack.HotModuleReplacementPlugin(),
+  new HtmlWebpackPlugin({
+    title: 'index.html',
+    template: join(__dirname, 'src', 'index.pug')
+  })
+]
+
+// Config object
+
+const config = {
+  devServer,
+  stats,
+  entry,
+  output,
+  module,
+  plugins
+}
+
+export default config
