d8f0901aab033a2ef60d4458c17000cb3146f3a8
Miguel Palau <mpalauzarza@gmail.com>
2017-09-11 00:08:37 -0500

    elegant urls bitch

--- webpack/config/devServer.js
+++ webpack/config/devServer.js
@@ -3,7 +3,10 @@ import { DIST } from './paths.js'
 const devServer = {
   hot: true,
   inline: true,
-  contentBase: DIST
+  contentBase: DIST,
+  staticOptions: {
+    extensions: ['html']
+  }
 }
 
 export default devServer
