63599eb5e40548fd41dd33cb42165f3ec3e17743
Miguel Palau <mpalauzarza@gmail.com>
2017-09-10 23:48:33 -0500

    need to fix the html-webpack-plugin

--- webpack/config/plugins.js
+++ webpack/config/plugins.js
@@ -4,22 +4,21 @@ import HtmlWebpackPlugin from 'html-webpack-plugin'
 import DashboardPlugin from 'webpack-dashboard/plugin'
 import { SRC, DIST } from './paths.js'
 import glob from 'glob'
-import HtmlWebpackHarddiskPlugin from 'html-webpack-harddisk-plugin'
 
 const pugFiles = glob.sync(SRC + '/*.pug')
 
-const pugPlugins = pugFiles.map(file => new HtmlWebpackPlugin({
-  filename: file.split('.pug')[0] + '.html',
-  template: file
-}))
+const pugPlugins = pugFiles.map(file => {
+  const html = file.split('.pug')[0] + '.html'
+  return new HtmlWebpackPlugin({
+    filename: html,
+    template: file
+  })
+})
 
 const plugins = [
   new DashboardPlugin(),
   new webpack.HotModuleReplacementPlugin(),
-  ...pugPlugins,
-  new HtmlWebpackHarddiskPlugin({
-    outputPath: DIST
-  })
+  ...pugPlugins
 ]
 
 export default plugins
