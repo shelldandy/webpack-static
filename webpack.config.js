38a50d4c127d81c36f303e1a0eaae64aff2c688b
Miguel Palau <mpalauzarza@gmail.com>
2017-12-05 00:22:43 -0600

    first success

--- webpack.config.js
+++ webpack.config.js
@@ -2,67 +2,34 @@ const {join} = require('path')
 const {cwd} = require('process')
 
 const SRC = join(cwd(), 'src')
-const BUILD = join(cwd(), 'src')
+const BUILD = join(cwd(), 'build')
 
 const index = join(SRC, 'index.pug')
+const sample = join(SRC, 'sample.pug')
 
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+let extractHtml = new ExtractTextPlugin('[name].html')
 
 module.exports = {
-  entry: index,
+  entry: {
+    index,
+    sample
+  },
   output: {
     path: BUILD,
     filename: '[name].js'
   },
   module: {
-    // your modules...
     rules: [
       {
         test: /\.pug$/,
-        loaders: ['pug-loader']
+        use: extractHtml.extract({
+          use: ['html-loader', 'pug-html-loader?pretty&exports=false']
+        })
       }
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      // Required
-      inject: false,
-      template: require('html-webpack-template-pug'),
-      // template: '!!pug-loader!node_modules/html-webpack-template-pug/layout.pug'
-
-      // Optional
-      appMountId: 'app',
-      // appMountId: ['app1', 'app2']
-      mobile: true,
-      injectExtras: {
-        head: [
-          'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css',
-          {
-            tag: 'link',
-            rel: 'dns-prefetch',
-            href: '//example.com/'
-          },
-          {
-            tag: 'base',
-            href: '../assets/page.html'
-          },
-          {
-            tag: 'meta',
-            name: 'description',
-            content: 'A description of the page'
-          }
-        ],
-        body: [
-          'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js',
-          'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js',
-          {
-            tag: 'noscript',
-            innerHTML: "JavaScript is disabled in your browser. <a href='http://www.enable-javascript.com/' target='_blank'>Here</a> is how to enable it."
-          }
-        ]
-      },
-      title: 'My App'
-      // Other html-webpack-plugin options...
-    })
+    extractHtml
   ]
 }
