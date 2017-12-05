a34a7ca0d10e7d5a80be4b8984123480db218dca
Miguel Palau <mpalauzarza@gmail.com>
2017-12-05 01:26:52 -0600

    adding sass loaders

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,19 @@ module.exports = {
             }
           ]
         })
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }]
       }
     ]
   },
